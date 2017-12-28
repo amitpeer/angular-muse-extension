af1f82ed2bebd74405dfdb5caf95c32d34c4e1e4
amitp <amitpeer05@gmail.com>
2017-12-28 21:35:45 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from "@angular/core";
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  DOWN_ARROW = 40
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // constructor() {
+  // }
+
+  letters = [["A", "B", "C", "D"],
+    ["E", "F", "G", "H"],
+    ["I", "J", "K", "L"],
+    ["M", "N", "O", "P"],
+    ["Q", "R", "S", "T"],
+    ["U", "V", "W", "X"],
+    ["Y", "Z", "?", "?"]];
+
+  selectorIndex = {row: 0, col: 0};
+
+  ngAfterViewInit() {
+    let row = this.selectorIndex.row;
+    let col = this.selectorIndex.col;
+    document.getElementById(row + ":" + col).style.backgroundColor = "yellow";
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event:KeyboardEvent) {
+    console.log(event);
+
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
+      this.selectorIndex.col++;
+      this.ngAfterViewInit()
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW) {
+      this.selectorIndex.col--;
+      this.ngAfterViewInit()
+    }
+  }
+
+
+
+
 }
---
+++ src/assets/painter.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  console.log("painter.js");
+
+  let abcLetters = ["A", "B", "C", "D", "E", "F", "G",
+    "H", "I", "J", "K", "L", "M", "N",
+    "O", "P", "Q", "R", "S", "T", "U",
+    "V", "W", "X", "Y", "Z"];
+
+  let aElements = $("a:visible");
+
+  for (let i = 0; i < abcLetters.length; i++) {
+    let letter = document.createTextNode(" " + abcLetters[i]);
+    let backgroundSpan = document.createElement("span");
+    backgroundSpan.style.backgroundColor = "red";
+    backgroundSpan.style.border = "thin dotted blue";
+    backgroundSpan.style.opacity = 0.6;
+
+    let letterSpan = document.createElement("span");
+    letterSpan.style.fontSize = "20px";
+    letterSpan.style.opacity = 1;
+    letterSpan.style.color = "green";
+
+    letterSpan.appendChild(letter);
+    backgroundSpan.appendChild(letterSpan);
+    aElements[i].appendChild(backgroundSpan);
+  }
+});
